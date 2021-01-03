cb9432e13cf5465a9a0408e5e74b77430d44d685
sauravDutt <sauravd069@gmail.com>
2021-01-03 17:46:30 +0530

    just added some more stuff

--- bot2/main.py
+++ bot2/main.py
@@ -3,17 +3,15 @@ import os
 
 userName = "sauravDutt"
 password = "sauravd069"
+quit = "quit"
+documentation = "doc"
+sauravDutt = "sd"
 
 def clear(): 
     os.system('clear') 
 
- 
-while True :
+def authentication(name):
     
-    name = input("Enter a User Name : \n")
-    clear()
-    if name != userName:
-        continue
     if name == userName:
         print("\n    *******************************************    ")
         print("                  Welcome back Saurav")
@@ -39,7 +37,7 @@ while True :
             print("                         doc  - Documentation                     " )
             print("                      MORE FUNCTIONS COMING SOON            ")
             print(" _____________________________________________________________________\n")
-            break
+            
         else:
             print("\n    *******************************************    ")
             print("    *********    _________________    *********    ")
@@ -50,22 +48,38 @@ while True :
             print("\n ************************************************* ")
             input("            PRESS ANY KEY TO RESTART     :-  ")
             clear()
-            continue
+def menu(userInput):
+    if userInput == quit :
+        clear()
+        print("\n ************************************************* ")
+        print("               Good Day, to you sir!")
+        print(" ************************************************* \n")
+        sys.exit()
+    elif userInput == documentation:
+        clear()
+        print("\n ***************************************************** \n")
+        print("              Documentation - SDtools")
+        print("   This Program provides you with Exclusive tools ")
+        print("  This is just a practice of python just tried to ")
+        print("  Make a switch case kind of applicatyion (or menu)\n")
+        print(" ****************************************************** \n")
+    else:
+        clear()
+        print("\n ************************************************* ")
+        print("             More features coming soon!!!")
+        print(" ************************************************* \n")
 
+while True :
+    
+    name = input("Enter a User Name : \n")
+    clear()
+    if name != userName:
+        continue
+    authentication(name)
+    userInput = input("            Enter your option  :-    ")
+    menu(userInput)
    
-userInput = input("            Enter your option  :-    ")
 
-quit = "quit"
-documentation = "doc"
-sauravDutt = "sd"
-if userInput == quit :
-    clear()
-    print("\n ************************************************* ")
-    print("               Good Day, to you sir!")
-    print(" ************************************************* \n")
-    sys.exit()
-else:
-    clear()
-    print("\n ************************************************* ")
-    print("             More features coming soon!!!")
-    print(" ************************************************* \n")
+
+
+
